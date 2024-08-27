efebbd0321869774724e26b32db0670bc03c2cc9
Efe Başaran <efebasaranofficial@gmail.com>
2024-08-27 13:53:04 +0300

    A fake data method was written that will work when entered incorrectly.

---
+++ S-Efe-BranchPR/FalseMethod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S_Efe_BranchPR
+{
+    public class FalseMethod
+    {
+        public void GetData()
+        {
+            bool Status = false;
+            string data = null;
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine($"İşlem Başarısız ");
+            Console.WriteLine(
+                $"{Status}"+
+                $"{data}"+
+                $"{dateTime}"
+                );
+        }
+    }
+}
