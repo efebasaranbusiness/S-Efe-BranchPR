e81595b3f859097de8930de29b8b7e109b5b003f
Efe Başaran <efebasaranofficial@gmail.com>
2024-08-27 13:48:32 +0300

    Created Method Called in ProgramCs

--- S-Efe-BranchPR/Program.cs
+++ S-Efe-BranchPR/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Lütfen Bir Sayı Giriniz");
+﻿using S_Efe_BranchPR;
+
+Console.WriteLine("Lütfen Bir Sayı Giriniz");
 int UserNumber = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Tahmin Edilecek Sayı");
@@ -6,7 +8,9 @@ int CatchNumber = Convert.ToInt32(Console.ReadLine());
 
 if (UserNumber == CatchNumber)
 {
-    Console.WriteLine("Sonuç Doğru");
+    Console.WriteLine("Sonuç Doğru İşte Bilgileriniz\n");
+    XMethodClass xMethodClass = new XMethodClass();
+    xMethodClass.GetUserData();
 }
 else
 {
